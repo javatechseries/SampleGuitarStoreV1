b8a5f68836243186cb5845183cac6502086c8be6
kotan <kotan@host.docker.internal>
2019-09-07 21:53:08 -0400

    Commented the SYSOUT's

--- SampleGuitarStoreV1/src/com/guitar/store/Inventory.java
+++ SampleGuitarStoreV1/src/com/guitar/store/Inventory.java
@@ -34,25 +34,25 @@ public class Inventory {
 			Guitar matchingGuitar= (Guitar)i.next();
 			
 			
-			System.out.println("Builder: "+matchingGuitar.getBuilder().toLowerCase()+"Customer Provided Builder: "+customerSearch.getBuilder().toLowerCase());
+			//System.out.println("Builder: "+matchingGuitar.getBuilder().toLowerCase()+"Customer Provided Builder: "+customerSearch.getBuilder().toLowerCase());
 			
 			if(!matchingGuitar.getBuilder().toLowerCase().equals(customerSearch.getBuilder().toLowerCase()))
 				continue;
 			
-			System.out.println("Model: "+matchingGuitar.getModel().toLowerCase()+"Customer Provided Model: "+customerSearch.getModel().toLowerCase());
+			//System.out.println("Model: "+matchingGuitar.getModel().toLowerCase()+"Customer Provided Model: "+customerSearch.getModel().toLowerCase());
 			
 			if(!matchingGuitar.getModel().toLowerCase().equals( customerSearch.getModel().toLowerCase()))
 				continue;
 			
-			System.out.println("Type: "+matchingGuitar.getType().toLowerCase()+"Customer Provided Type: "+customerSearch.getType().toLowerCase());
+			//System.out.println("Type: "+matchingGuitar.getType().toLowerCase()+"Customer Provided Type: "+customerSearch.getType().toLowerCase());
 			if(!matchingGuitar.getType().toLowerCase().equals(customerSearch.getType().toLowerCase()))
 				continue;
 			
-			System.out.println("BackWood: "+matchingGuitar.getBackWood().toLowerCase()+"Customer Provided backWood: "+customerSearch.getBackWood().toLowerCase());
+			//System.out.println("BackWood: "+matchingGuitar.getBackWood().toLowerCase()+"Customer Provided backWood: "+customerSearch.getBackWood().toLowerCase());
 			if(!matchingGuitar.getBackWood().toLowerCase().equals(customerSearch.getBackWood().toLowerCase()))
 				continue;
 			
-			System.out.println("TopWood: "+matchingGuitar.getTopWood().toLowerCase()+"Customer Provided TopWood: "+customerSearch.getTopWood().toLowerCase());
+			//System.out.println("TopWood: "+matchingGuitar.getTopWood().toLowerCase()+"Customer Provided TopWood: "+customerSearch.getTopWood().toLowerCase());
 			if(!matchingGuitar.getTopWood().toLowerCase().equals(customerSearch.getTopWood().toLowerCase()))
 				continue;
 			
